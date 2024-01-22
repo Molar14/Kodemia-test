be821c368968394bf4ad71eccae3a12245e23b50
Molar14 <grubenmj@gmail.com>
2024-01-22 13:33:44 -0600

    feat:There was some test about CSS and JS also  the backround image was change and its now directed from CSS

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+document.getElementById("a").innerHTML="Prueba";
+window.alert("No es broma");
+window.alert("Madres calientes cerca de tu casa");
+window.alert("No es broma");
+window.alert("Laura te espera");
